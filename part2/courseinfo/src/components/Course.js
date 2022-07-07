46f72d92286d109c607a618f39060cef4f414468
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-07 17:59:51 +0200

    Exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -3,13 +3,20 @@ const Header = ({name}) => {
 }
 
 const Content = ({parts}) => {
-    return parts.map(part => (
+    const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
+
+    return (
+    <>
+        {parts.map(part => (
         <Part 
             key={part.id}
             name={part.name}
             exercises={part.exercises}
         />
-    ))
+        ))}
+        <strong>total of {total} exercises</strong>
+    </>
+    )
 }
 
 const Part = ({name, exercises}) => {
@@ -22,7 +29,6 @@ const Course = ({course}) => {
         <>
             <Header name={course.name}/>
             <Content parts={course.parts}/>
-            
         </>
     )
 }
