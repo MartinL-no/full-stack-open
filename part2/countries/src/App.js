7a73277b7213ddc9345bc330a2a69d996cb6a5b8
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-09 17:07:02 +0200

    Exercise 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CountrySummary = (props) => {
+  const countryData = props.filtered[0]
+  const languages = Object.values(countryData.languages)
+  console.log(languages)
+  console.log(countryData)
+
+    return (
+      <>
+        <h1>{countryData.name.common}</h1>
+        <p>capital {countryData.capital}</p>
+        <p>area {countryData.area}</p>
+      
+        <strong>languages:</strong>
+  
+        <ul>
+          {languages.map(language => (
+            <li key={language}>{language}</li>))}
+        </ul>
+  
+        <img src={countryData.flags.png}/>
+      </>
+    )
+  console.log(test)
 }
 
-export default App;
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filtered, setFiltered] = useState([])
+  const [value, setValue] = useState("")
+
+  useEffect(()=> {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(res => {
+        setCountries(res.data)
+      })
+  }, [])
+
+  function handleChange(event) {
+    setValue(event.target.value)
+    setFiltered(countries.filter(country => country.name.common.toLowerCase().includes(value.toLowerCase())))
+  }
+
+  const showCountries = filtered.length === 1
+    ? <CountrySummary filtered={filtered}/>
+    : filtered.map(country => <p key={country.name.common}>{country.name.common}</p>)
+
+  return (
+    <div>
+      <span>find countries</span>
+      <input
+        value={value}
+        onChange={handleChange}
+      />
+      {showCountries}
+    </div>
+  )
+}
+
+export default App
