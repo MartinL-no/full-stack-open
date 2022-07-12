0fa13bba1fa11283fb110001cee9259d83d22792
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-12 19:59:51 +0200

    Exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,29 +1,41 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    console.log(res.req.body)
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        `${JSON.stringify(res.req.body)}`
+    ].join(' ')
+  }))
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
@@ -44,8 +56,8 @@ app.get(`/api/persons/:id`, (request, response) => {
 app.get(`/info`, (request, response) => {
     const date = new Date()
     response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
     `)
 })
 
@@ -54,7 +66,7 @@ app.post(`/api/persons`, (request, response) => {
     const duplicate = persons.some(person => {
         return person.name.toLowerCase() === personDetails.name.toLowerCase()
     })
-
+    
     if (personDetails.name && personDetails.number && !duplicate) {
         persons = persons.concat({
             id: Math.floor(Math.random() * 1000),
@@ -62,7 +74,6 @@ app.post(`/api/persons`, (request, response) => {
             number: personDetails.number
         })
         response.json(personDetails)
-        console.log(persons)
     } else if (!personDetails.name) {
         response.status(400).json({ 
             error: 'name must be included' 
@@ -81,11 +92,11 @@ app.post(`/api/persons`, (request, response) => {
 app.delete(`/api/persons/:id`, (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-
+    
     response.status(204).end()
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
