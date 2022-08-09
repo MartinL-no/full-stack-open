0383b8df94aa203db777bd023b197bae0d0de967
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-08-09 15:55:26 +0200

    Working

---
+++ part4/bloglist/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+const Blog = require('./models/blog')
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,32 +1,10 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
 
-app.use(cors())
-app.use(express.json())
-const Blog = require('./models/blog')
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
+const PORT = config.PORT
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
