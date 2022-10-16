7327a12cd0f727aa4a862da3b1785c829d36ed3d
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-10-16 19:49:21 +0200

    Exercise 4.17

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,10 +1,9 @@
 const blogRouter = require('express').Router()
-const { notEqual } = require('assert')
 const Blog = require('../models/blog')
-const { blogsInDb } = require('../tests/test_helper')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
@@ -12,16 +11,22 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const users = await User.find({})
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: users[0]._id
   })
 
   try {  
     const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+    users[0].blogs = users[0].blogs.concat(savedBlog._id)
+    await users[0].save()
+
+    response.json(savedBlog)
   } catch(exception) {
     next(exception)
   }
