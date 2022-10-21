5d93bb98e3219c58252fd93a05663aab454b9af1
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-10-21 13:02:06 +0200

    Add describe block

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,78 +5,65 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test(
-  'renders the blog`s title and author, but does not url or number of likes by default',
-  () => {
-    const blog = {
-      title: 'Test Blog',
-      author: 'Test Author',
-      url: 'https://test.com/',
-      likes: 99
+describe('<Blog />', () => {
+  let blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://test.com/',
+    likes: 99,
+    user: {
+      name: 'Test'
     }
-
-    const { container } = render(<Blog blog={blog} />)
-
-    const div = container.querySelector('.blog')
-
-    expect(div).toHaveTextContent('Test Blog')
-    expect(div).toHaveTextContent('Test Author')
-    expect(div).not.toHaveTextContent('https://test.com/')
-    expect(div).not.toHaveTextContent(99)
   }
-)
 
-test('url and number of likes are shown when the button controlling the shown details has been clicked',
-  async () => {
-    const blog = {
-      title: 'Test Blog',
-      author: 'Test Author',
-      url: 'https://test.com/',
-      likes: 99,
-      user: {
-        name: 'Test'
-      }
+  test(
+    'renders the blog`s title and author, but does not url or number of likes by default',
+    () => {
+      const { container } = render(<Blog blog={blog} />)
+
+      const div = container.querySelector('.blog')
+
+      expect(div).toHaveTextContent('Test Blog')
+      expect(div).toHaveTextContent('Test Author')
+      expect(div).not.toHaveTextContent('https://test.com/')
+      expect(div).not.toHaveTextContent(99)
     }
+  )
 
-    const { container } = render(<Blog blog={blog} />)
+  test('url and number of likes are shown when the button controlling the shown details has been clicked',
+    async () => {
+      const { container } = render(<Blog blog={blog} />)
 
-    const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
+      const user = userEvent.setup()
+      const button = screen.getByText('view')
+      await user.click(button)
 
-    const div = container.querySelector('.blog')
+      const div = container.querySelector('.blog')
 
-    expect(div).toHaveTextContent('https://test.com/')
-    expect(div).toHaveTextContent('99')
-  }
-)
-
-test('if the like button is clicked twice, the event handler the component received as props is called twice',
-  async () => {
-    const blog = {
-      title: 'Test Blog',
-      author: 'Test Author',
-      url: 'https://test.com/',
-      likes: 0,
-      user: {
-        name: 'Test'
-      }
+      expect(div).toHaveTextContent('https://test.com/')
+      expect(div).toHaveTextContent('99')
     }
+  )
 
-    const mockHandler = jest.fn()
+  test('if the like button is clicked twice, the event handler the component received as props is called twice',
+    async () => {
+      const mockHandler = jest.fn()
 
-    render(
-      <Blog blog={blog} addLike={mockHandler} />
-    )
+      render(
+        <Blog blog={blog} addLike={mockHandler} />
+      )
 
-    const user = userEvent.setup()
-    const viewButton = screen.getByText('view')
-    await user.click(viewButton)
-    const likeButton = screen.getByText('like')
-    await user.click(likeButton)
-    await user.click(likeButton)
+      const user = userEvent.setup()
+      const viewButton = screen.getByText('view')
+      await user.click(viewButton)
+      const likeButton = screen.getByText('like')
+      await user.click(likeButton)
+      await user.click(likeButton)
 
-    expect(mockHandler.mock.calls).toHaveLength(2)
-  }
-)
+      expect(mockHandler.mock.calls).toHaveLength(2)
+    }
+  )
+})
+
+
 
