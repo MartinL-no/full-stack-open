dea7a70b21538475562efe253be178cae0554105
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-10-22 12:16:00 +0200

    Exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/note_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/note_app.cy.js
@@ -1,12 +1,42 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Superuser',
+      username: 'root',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username-input').type('root')
+      cy.get('#password-input').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Superuser logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username-input').type('root')
+      cy.get('#password-input').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Superuser logged in')
+    })
   })
 })
 
