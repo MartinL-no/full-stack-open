cdf6c9671e66ddd58b8bc69b43a0c5118ae263eb
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-11-25 16:30:27 +0100

    Exercise 9.10

---
+++ part9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,11 @@
+import diagnoses from '../data/diagnoses';
+
+import { DiagnosesEntry } from '../types';
+
+const getEntries = (): Array<DiagnosesEntry> => {
+  return diagnoses;
+};
+
+export default {
+  getEntries
+};
