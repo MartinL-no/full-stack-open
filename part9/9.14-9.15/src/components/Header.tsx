ee9a2bc6c3175ff5b720faaa591f835271aaaea6
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-11-26 15:30:24 +0100

    Exercise 9.14

---
+++ part9/9.14-9.15/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = ({ courseName }: { courseName: string}) => {
+  return (
+    <>
+      <h1>{courseName}</h1>
+
+    </>
+  )
+
+};
+
+export default Header
---
+++ part9/9.14-9.15/src/components/Total.tsx
@@ -0,0 +1,18 @@
+interface CoursePart {
+  name: string
+  exerciseCount: number
+}
+
+const Total = ({ courseParts }: { courseParts: Array<CoursePart> }) => {
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </>
+  )
+
+};
+
+export default Total
