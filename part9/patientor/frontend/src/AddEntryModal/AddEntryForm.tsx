9bc142c31669cdde5707022463781e1e1b3b92e4
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-11-30 13:04:54 +0100

    Exercise 9.26

--- part9/patientor/frontend/src/AddEntryModal/AddEntryForm.tsx
+++ part9/patientor/frontend/src/AddEntryModal/AddEntryForm.tsx
@@ -4,7 +4,7 @@ import { Grid, Button } from "@material-ui/core";
 import { Field, Formik, Form } from "formik";
 
 import { TextField, SelectField, EntryTypeOption, DiagnosisSelection } from "./FormField";
-import { NewEntry, EntryType } from "../types";
+import { Diagnoses, NewEntry, EntryType, HealthCheckRating } from "../types";
 
 export type EntryFormValues = NewEntry;
 
@@ -20,26 +20,51 @@ const entryTypeOptions: EntryTypeOption[] = [
   { value: EntryType.Hospital, label: "Hospital" },
 ];
 
+interface allEntryFields {
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnoses['code']>;
+  type: EntryType;
+  discharge: {
+    date: "",
+    criteria: ""
+  };
+  employerName: string;
+  sickLeave: {
+    startDate: string
+    endDate: string
+  };
+  healthCheckRating: HealthCheckRating;
+}
+
 export const AddEntryForm = ({ onSubmit, onCancel, setError }: Props) => {
   const [{ diagnoses }] = useStateValue();
-
+  const initialValues: allEntryFields = {
+    type: EntryType.Hospital,
+    description: "",
+    specialist: "",
+    diagnosisCodes: [],
+    date: "",
+    discharge: {
+      date: "",
+      criteria: ""
+    },
+    employerName: "",
+    sickLeave: {
+      startDate: "",
+      endDate: ""
+    },
+    healthCheckRating: HealthCheckRating.Healthy
+  };
   return (
     <Formik
-      initialValues={{
-        type: EntryType.Hospital,
-        description: "",
-        specialist: "",
-        diagnosisCodes: [],
-        date: "",
-        discharge: {
-          date: "",
-          criteria: ""
-        }
-      }}
+      initialValues={initialValues}
       onSubmit={onSubmit}
       validate={(values) => {
         const requiredError = "Field is required";
-        const errors: { [field: string]: string } = {};
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const errors: any = {};
         if (!values.type) {
           errors.type = requiredError;
         }
@@ -63,75 +88,179 @@ export const AddEntryForm = ({ onSubmit, onCancel, setError }: Props) => {
           setTimeout(() => setError(""), 5000);
           errors.date = "Date is incorrect format";
         }
+        if (values.type === "OccupationalHealthcare" && !values.employerName) {
+          setError("Employer name is required");
+          setTimeout(() => setError(""), 5000);
+          errors.employerName = requiredError;
+        }
+        if (values.type === "OccupationalHealthcare" && !values.sickLeave.startDate) {
+          setError("Sick leave start date field is required");
+          setTimeout(() => setError(""), 5000);
+          errors.sickLeave.startDate = requiredError;
+        }
+        if (values.type === "OccupationalHealthcare" && !Date.parse(values.sickLeave.startDate)) {
+          setError("Sick leave start date is incorrect format");
+          setTimeout(() => setError(""), 5000);
+          errors.sickLeave.startDate = "Date is incorrect format";
+        }
+        if (values.type === "OccupationalHealthcare" && !values.sickLeave.endDate) {
+          setError("Sick leave end date field is required");
+          setTimeout(() => setError(""), 5000);
+          errors.sickLeave.endDate = requiredError;
+        }
+        if (values.type === "OccupationalHealthcare" && !Date.parse(values.sickLeave.endDate)) {
+          setError("Sick leave end date is incorrect format");
+          setTimeout(() => setError(""), 5000);
+          errors.sickLeave.endDate = "Date is incorrect format";
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return errors;
       }}
     >
-      {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
-        return (
-          <Form className="form ui">
-            <SelectField label="Type" name="type" options={entryTypeOptions} />
-            <Field
-              label="Description"
-              placeholder="Description"
-              name="description"
-              component={TextField}
-            />
-            <Field
-              label="Specialist"
-              placeholder="Specialist"
-              name="specialist"
-              component={TextField}
-            />
-            <DiagnosisSelection
-              setFieldValue={setFieldValue}
-              setFieldTouched={setFieldTouched}
-              diagnoses={Object.values(diagnoses)}
-            />
-            <Field
-              label="Date"
-              placeholder="YYYY-MM-DD"
-              name="date"
-              component={TextField}
-            />
-            <Field
-              label="Discharge Date"
-              placeholder="YYYY-MM-DD"
-              name="discharge.date"
-              component={TextField}
-            />
-            <Field
-              label="Discharge Criteria"
-              placeholder="Discharge Criteria"
-              name="discharge.criteria"
-              component={TextField}
-            />
-            <Grid>
-              <Grid item>
-                <Button
-                  color="secondary"
-                  variant="contained"
-                  style={{ float: "left" }}
-                  type="button"
-                  onClick={onCancel}
-                >
-                  Cancel
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button
-                  style={{
-                    float: "right",
-                  }}
-                  type="submit"
-                  variant="contained"
-                  disabled={!dirty || !isValid}
-                >
-                  Add
-                </Button>
-              </Grid>
-            </Grid>
-          </Form>
-        );
+      {({ isValid, dirty, setFieldValue, setFieldTouched, values }) => {
+        console.log(values);
+        switch (values.type) {
+          case "Hospital":
+            return (
+              <Form className="form ui">
+                <SelectField label="Type" name="type" options={entryTypeOptions} />
+                <Field
+                  label="Description"
+                  placeholder="Description"
+                  name="description"
+                  component={TextField}
+                />
+                <Field
+                  label="Specialist"
+                  placeholder="Specialist"
+                  name="specialist"
+                  component={TextField}
+                />
+                <DiagnosisSelection
+                  setFieldValue={setFieldValue}
+                  setFieldTouched={setFieldTouched}
+                  diagnoses={Object.values(diagnoses)}
+                />
+                <Field
+                  label="Date"
+                  placeholder="YYYY-MM-DD"
+                  name="date"
+                  component={TextField}
+                />
+                <Field
+                  label="Discharge Date"
+                  placeholder="YYYY-MM-DD"
+                  name="discharge.date"
+                  component={TextField}
+                />
+                <Field
+                  label="Discharge Criteria"
+                  placeholder="Discharge Criteria"
+                  name="discharge.criteria"
+                  component={TextField}
+                />
+                <Grid>
+                  <Grid item>
+                    <Button
+                      color="secondary"
+                      variant="contained"
+                      style={{ float: "left" }}
+                      type="button"
+                      onClick={onCancel}
+                    >
+                      Cancel
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      style={{
+                        float: "right",
+                      }}
+                      type="submit"
+                      variant="contained"
+                      disabled={!dirty || !isValid}
+                    >
+                      Add
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            );
+          case "OccupationalHealthcare":
+            return (
+              <Form className="form ui">
+                <SelectField label="Type" name="type" options={entryTypeOptions} />
+                <Field
+                  label="Description"
+                  placeholder="Description"
+                  name="description"
+                  component={TextField}
+                />
+                <Field
+                  label="Specialist"
+                  placeholder="Specialist"
+                  name="specialist"
+                  component={TextField}
+                />
+                <DiagnosisSelection
+                  setFieldValue={setFieldValue}
+                  setFieldTouched={setFieldTouched}
+                  diagnoses={Object.values(diagnoses)}
+                />
+                <Field
+                  label="Date"
+                  placeholder="YYYY-MM-DD"
+                  name="date"
+                  component={TextField}
+                />
+                <Field
+                  label="Employer Name"
+                  placeholder="Employer Name"
+                  name="employerName"
+                  component={TextField}
+                />
+                <Field
+                  label="Sick Leave:  start date"
+                  placeholder="YYYY-MM-DD"
+                  name="sickLeave.startDate"
+                  component={TextField}
+                />
+                <Field
+                  label="Sick Leave: end date"
+                  placeholder="YYYY-MM-DD"
+                  name="sickLeave.endDate"
+                  component={TextField}
+                />
+                <Grid>
+                  <Grid item>
+                    <Button
+                      color="secondary"
+                      variant="contained"
+                      style={{ float: "left" }}
+                      type="button"
+                      onClick={onCancel}
+                    >
+                      Cancel
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button
+                      style={{
+                        float: "right",
+                      }}
+                      type="submit"
+                      variant="contained"
+                      disabled={!dirty || !isValid}
+                    >
+                      Add
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            );
+          default:
+            break;
+        }
       }}
     </Formik>
   );
