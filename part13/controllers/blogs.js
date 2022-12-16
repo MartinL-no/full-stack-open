7f487960059718ea6a97f15bb34cb4c039a49e82
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-16 21:03:03 +0100

    Exercise 13.5

---
+++ part13/controllers/blogs.js
@@ -0,0 +1,39 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.note = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs =  await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.note) {
+    res.json(req.note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy()
+  }
+  res.status(204).end()
+})
+
+module.exports = router
