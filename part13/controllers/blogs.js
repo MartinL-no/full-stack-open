dfb8e5f5391f9997115c181da2594f6a7a8a09e6
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-18 14:45:17 +0100

    Exercise 13.14

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -5,11 +5,14 @@ const { userExtractor, blogFinder } = require('../util/middleware')
 const { Blog, User } = require('../models')
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
+    where = {
+      [Op.or]: [
+        { title: { [Op.iLike]: `%${req.query.search}%` }},
+        { author: { [Op.iLike]: `%${req.query.search}%` }},
+      ]
     }
   }
 
