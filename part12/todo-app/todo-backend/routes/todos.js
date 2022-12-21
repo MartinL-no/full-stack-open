b1b93b145ef78184cd364c7a40e5d7954c9ac3eb
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-21 15:05:07 +0100

    Exercise 12.7

--- part12/todo-app/todo-backend/routes/todos.js
+++ part12/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const body = req.body
+
+  req.todo.done = body.done
+  req.todo.save()
+
+  res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
